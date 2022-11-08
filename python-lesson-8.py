62ee36e55784912bf5b8c87e18d497bc44cfd993
Yen Giang Nguyen <103374998+yengiang@users.noreply.github.com>
2022-11-08 08:21:35 +0700

    Update python-lesson-8.py

--- python-lesson-8.py
+++ python-lesson-8.py
@@ -35,16 +35,10 @@ Fill in the body of the `blackjack_hand_greater_than` function according to the 
 
 def prettify_graph(graph):
     graph.set_title("Results of 500 slot machine pulls")
-    # Make the y-axis begin at 0
     graph.set_ylim(bottom=0)
-    # Label the y-axis
     graph.set_ylabel("Balance")
-    # Bonus: format the numbers on the y-axis as dollar amounts
-    # An array of the values displayed on the y-axis (150, 175, 200, etc.)
     ticks = graph.get_yticks()
-    # Format those values into strings beginning with dollar sign
     new_labels = ['${}'.format(int(amt)) for amt in ticks]
-    # Set the new labels
     graph.set_yticklabels(new_labels)
     
 graph = jimmy_slots.get_graph()
